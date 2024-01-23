dea17444e5a28852f8bb011c6246106dd30eb5b9
ArturBura <154540623+ArturBura@users.noreply.github.com>
2024-01-23 22:08:47 +0200

    Add files via upload

---
+++ Компьюктерный практикум 14/Компьюктерный практикум 14/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите сумму вклада");
+decimal a = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите кол-во месяцев");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= b)
+{
+    a += a * 0.07m;
+    i += 1;
+}
+Console.WriteLine($"Сумма по окончанию вашего вклада составит:{a}");
